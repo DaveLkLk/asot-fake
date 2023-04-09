d79f1173cc94dead046a89a4eb71047b77d7d008
Kevin Apaza <106572465+DaveLkLk@users.noreply.github.com>
2023-04-09 00:50:38 +0000

    cambios al usuario y password en database

--- php/database.php
+++ php/database.php
@@ -4,12 +4,12 @@
 
     $ssl = '/etc/ssl/certs/ca-certificates.crt';
     $server_name = 'aws.connect.psdb.cloud';
-    $user_name = '0xrgx2hmxtzreesxvm5b';
-    $db_password = 'pscale_pw_r7THUk5BZQ077Bpt9E6z2oxOFKQZxUC7k0S8S83D3WQ';
+    $user_name = '2bapwwfi84jda7lm5xd7';
+    $db_password = 'pscale_pw_8zW1odsx1gjsqAbW59RUOUIGynuFopzSe42uGE7yTgX';
     $db_name = 'asot-db';
 
     $mysqli = mysqli_init();
-    $mysqli = new mysqli();
+    // $mysqli = new mysqli();
     $mysqli->ssl_set(NULL, NULL, $ssl, NULL, NULL);
     $mysqli->real_connect($server_name, $user_name, $db_password, $db_name);
   
